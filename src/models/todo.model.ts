195f639ed8d3d29f8c4558f07ed340ca6a8991c0
Iury F. A. Ferreira <iury.franklinferreira@gmail.com>
2021-05-20 12:32:57 -0300

    ✨ add model

---
+++ src/models/todo.model.ts
@@ -0,0 +1,11 @@
+export class Todo {
+    id:number
+    text: string
+    done: boolean
+
+    constructor (id: number, text : string, done: boolean) {
+      this.id = id
+      this.text = text
+      this.done = done
+    }
+}
