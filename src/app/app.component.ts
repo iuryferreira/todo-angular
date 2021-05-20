45b378abb36137ed2caf0988743e1ecd80deda82
Iury F. A. Ferreira <iury.franklinferreira@gmail.com>
2021-05-20 12:32:33 -0300

    ✨ add app component behavior

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core'
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { Todo } from 'src/models/todo.model'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,56 @@ import { Component } from '@angular/core'
   styles: []
 })
 export class AppComponent {
-  title = 'todo-angular'
+  todos : Todo[] = []
+  form : FormGroup
+
+  constructor (private formBuilder : FormBuilder) {
+    this.form = this.formBuilder.group({
+      text: ['', Validators.required]
+    })
+    this.load()
+  }
+
+  done (todo : Todo) {
+    todo.done = true
+    this.save()
+  }
+
+  undone (todo : Todo) {
+    todo.done = false
+    this.save()
+  }
+
+  add () {
+    const text = this.form.controls.text.value
+    this.todos.push(new Todo(this.todos.length + 1, text, false))
+    this.save()
+    this.clear()
+  }
+
+  remove (todo : Todo) {
+    const index = this.todos.indexOf(todo)
+    if (index !== -1) {
+      this.todos.splice(index, 1)
+    }
+    this.save()
+  }
+
+  clear () {
+    this.form.reset()
+  }
+
+  save () {
+    const data = JSON.stringify(this.todos)
+    localStorage.setItem('todos', data)
+  }
+
+  load () {
+    const data = localStorage.getItem('todos')
+    if (data) {
+      this.todos = JSON.parse(data)
+    } else {
+      this.todos = []
+    }
+  }
 }
